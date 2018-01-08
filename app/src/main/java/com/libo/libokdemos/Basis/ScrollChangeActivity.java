bcabcd541c13937c65bbff8ab383c5537532208c
libolf <1021211327@qq.com>
2018-01-08 18:00:45 +0800

    联动效果—透明状态栏1

--- app/src/main/java/com/libo/libokdemos/Basis/ScrollChangeActivity.java
+++ app/src/main/java/com/libo/libokdemos/Basis/ScrollChangeActivity.java
@@ -11,6 +11,7 @@ import com.libo.libokdemos.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+// TODO: 2018-01-08 自定义Behavior http://blog.csdn.net/tabolt/article/details/51821933
 public class ScrollChangeActivity extends AppCompatActivity {
 
     @BindView(R.id.scroll_tool)
@@ -20,7 +21,7 @@ public class ScrollChangeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//此FLAG可使状态栏透明，且当前视图在绘制时，从屏幕顶端开始即top = 0开始绘制，这也是实现沉浸效果的基础
+//            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//此FLAG可使状态栏透明，且当前视图在绘制时，从屏幕顶端开始即top = 0开始绘制，这也是实现沉浸效果的基础
 //            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);//可不加
         }
         setContentView(R.layout.activity_scroll_change);
