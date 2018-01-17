d6c33db1fc2bc743d91040757613315dc2893d07
libolf <1021211327@qq.com>
2018-01-17 17:56:50 +0800

    运行时权限

--- app/src/main/java/com/libo/libokdemos/Basis/ContentProviderActivity.java
+++ app/src/main/java/com/libo/libokdemos/Basis/ContentProviderActivity.java
@@ -1,9 +1,17 @@
 package com.libo.libokdemos.Basis;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.libo.libokdemos.R;
 
@@ -29,8 +37,28 @@ public class ContentProviderActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.permission:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:10086"));
+                    startActivity(intent);
+                } 
                 break;
             case R.id.content_provider:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "权限申请成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "权限申请拒绝", Toast.LENGTH_SHORT).show();
+                } 
                 break;
         }
     }
