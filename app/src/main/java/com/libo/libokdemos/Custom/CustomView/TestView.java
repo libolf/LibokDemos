67e7ef28cc952c008da494f373ee2d8a99f23f0a
libolf <1021211327@qq.com>
2018-01-19 18:00:43 +0800

    AlertDialog工具

---
+++ app/src/main/java/com/libo/libokdemos/Custom/CustomView/TestView.java
@@ -0,0 +1,88 @@
+package com.libo.libokdemos.Custom.CustomView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.libo.libokdemos.Utils.DisplayUtils;
+
+/**
+ * Created by libok on 2018-01-19.
+ */
+
+public class TestView extends View {
+
+    private static final String TAG = "TestView";
+
+    private Context mContext;
+
+    public TestView(Context context) {
+        this(context, null);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec));
+    }
+
+    private int measureWidth(int widthMeasureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(widthMeasureSpec);
+        int specSize = MeasureSpec.getSize(widthMeasureSpec);
+        Log.e(TAG, "measureWidth: " + specSize);
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = 200; //DisplayUtils.dp2px(mContext, 200);
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
+    private int measureHeight(int heightMeasureSpec) {
+        int result = 0;
+        int specMode = MeasureSpec.getMode(heightMeasureSpec);
+        int specSize = MeasureSpec.getSize(heightMeasureSpec);
+        Log.e(TAG, "measureHeight: " + specSize);
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize;
+        } else {
+            result = 100; //DisplayUtils.dp2px(mContext, 200);
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+}
